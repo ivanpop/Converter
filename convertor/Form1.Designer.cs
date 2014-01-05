030493d4b0ecffbf6e63cd53dcb3afb0615b36e4
ivanpop <ivanpop@abv.bg>
2014-01-05 18:41:15 +0200

    Simple unit converter

---
+++ convertor/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace convertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kgBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbsBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inchesBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.feetBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.feetInchBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.mphBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.kmhBox = new System.Windows.Forms.TextBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kgBox
+            // 
+            this.kgBox.Location = new System.Drawing.Point(12, 34);
+            this.kgBox.Name = "kgBox";
+            this.kgBox.Size = new System.Drawing.Size(54, 20);
+            this.kgBox.TabIndex = 0;
+            this.kgBox.Text = "0";
+            this.kgBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "kg to lbs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "lbs to kg";
+            // 
+            // lbsBox
+            // 
+            this.lbsBox.Location = new System.Drawing.Point(12, 60);
+            this.lbsBox.Name = "lbsBox";
+            this.lbsBox.Size = new System.Drawing.Size(54, 20);
+            this.lbsBox.TabIndex = 2;
+            this.lbsBox.Text = "0";
+            this.lbsBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "inches to cm";
+            // 
+            // inchesBox
+            // 
+            this.inchesBox.Location = new System.Drawing.Point(12, 112);
+            this.inchesBox.Name = "inchesBox";
+            this.inchesBox.Size = new System.Drawing.Size(54, 20);
+            this.inchesBox.TabIndex = 6;
+            this.inchesBox.Text = "0";
+            this.inchesBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "cm to inches";
+            // 
+            // cmBox
+            // 
+            this.cmBox.Location = new System.Drawing.Point(12, 86);
+            this.cmBox.Name = "cmBox";
+            this.cmBox.Size = new System.Drawing.Size(54, 20);
+            this.cmBox.TabIndex = 4;
+            this.cmBox.Text = "0";
+            this.cmBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(161, 37);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(85, 70);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.resultLabel.ForeColor = System.Drawing.Color.Red;
+            this.resultLabel.Location = new System.Drawing.Point(6, 221);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 25);
+            this.resultLabel.TabIndex = 10;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // feetBox
+            // 
+            this.feetBox.Location = new System.Drawing.Point(12, 138);
+            this.feetBox.Name = "feetBox";
+            this.feetBox.Size = new System.Drawing.Size(22, 20);
+            this.feetBox.TabIndex = 11;
+            this.feetBox.Text = "0";
+            this.feetBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label8.Location = new System.Drawing.Point(35, 136);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(11, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "\'";
+            // 
+            // feetInchBox
+            // 
+            this.feetInchBox.Location = new System.Drawing.Point(44, 138);
+            this.feetInchBox.Name = "feetInchBox";
+            this.feetInchBox.Size = new System.Drawing.Size(22, 20);
+            this.feetInchBox.TabIndex = 13;
+            this.feetInchBox.Text = "0";
+            this.feetInchBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label9.Location = new System.Drawing.Point(67, 136);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "\"";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(80, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "feet to cm";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(71, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "mph to kmh";
+            // 
+            // mphBox
+            // 
+            this.mphBox.Location = new System.Drawing.Point(11, 190);
+            this.mphBox.Name = "mphBox";
+            this.mphBox.Size = new System.Drawing.Size(54, 20);
+            this.mphBox.TabIndex = 18;
+            this.mphBox.Text = "0";
+            this.mphBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "kmh to mph";
+            // 
+            // kmhBox
+            // 
+            this.kmhBox.Location = new System.Drawing.Point(11, 164);
+            this.kmhBox.Name = "kmhBox";
+            this.kmhBox.Size = new System.Drawing.Size(54, 20);
+            this.kmhBox.TabIndex = 16;
+            this.kmhBox.Text = "0";
+            this.kmhBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // closeButton
+            // 
+            this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.closeButton.Location = new System.Drawing.Point(161, 115);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(85, 23);
+            this.closeButton.TabIndex = 20;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(258, 24);
+            this.menuStrip1.TabIndex = 22;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calculateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.closeButton;
+            this.ClientSize = new System.Drawing.Size(258, 253);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.mphBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.kmhBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.feetInchBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.feetBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inchesBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbsBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kgBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Converter 1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox kgBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lbsBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inchesBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox cmBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox feetBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox feetInchBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox mphBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox kmhBox;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ convertor/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convertor
+{
+    public partial class Form1 : Form
+    {
+        double a, b;
+        bool result1, result2;
+
+        public void nullify()
+        {
+            kgBox.Text = "0";
+            lbsBox.Text = "0";
+            cmBox.Text = "0";
+            inchesBox.Text = "0";
+            feetBox.Text = "0";
+            feetInchBox.Text = "0";
+            kmhBox.Text = "0";
+            mphBox.Text = "0";
+        }
+
+        public void error_msg()
+        {
+            resultLabel.Text = "Wrong input!";
+            nullify();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result1 = Double.TryParse(kgBox.Text, out a);
+            if (!result1) error_msg();            
+
+            if (a != 0)
+            {
+                a *= 2.204622622;
+                a = System.Math.Round(a, 1);
+                resultLabel.Text = kgBox.Text + " kg is: " + System.Convert.ToString(a) + " pounds.";
+                nullify();
+            }
+
+            result1 = Double.TryParse(lbsBox.Text, out a);
+            if (!result1) error_msg(); 
+
+            if (a != 0)
+            {
+                a *= 0.45359237;
+                a = System.Math.Round(a, 1);
+                resultLabel.Text = lbsBox.Text + " lbs is: " + System.Convert.ToString(a) + " kg.";
+                nullify();
+            }
+
+            result1 = Double.TryParse(cmBox.Text, out a);
+            if (!result1) error_msg(); 
+
+            if (a != 0)
+            {
+                a *= 0.3937008;
+                a = System.Math.Round(a, 1);
+                resultLabel.Text = cmBox.Text + " cm is: " + System.Convert.ToString(a) + " inches.";
+                nullify();
+            }
+
+            result1 = Double.TryParse(inchesBox.Text, out a);
+            if (!result1) error_msg(); 
+
+            if (a != 0)
+            {
+                a *= 2.54;
+                a = System.Math.Round(a, 1);
+                resultLabel.Text = inchesBox.Text + " inches is: " + System.Convert.ToString(a) + " cm.";
+                nullify();
+            }
+
+            result1 = Double.TryParse(feetBox.Text, out a);
+            result2 = Double.TryParse(feetInchBox.Text, out b);
+            if (!result1 || !result2) error_msg();
+
+            if (a != 0)
+            {
+                a *= 30.48;
+                b *= 2.54;
+                a += b;
+                a = System.Math.Round(a, 1);
+                resultLabel.Text = feetBox.Text + "' " + feetInchBox.Text + "'' is " + System.Convert.ToString(a) + " cm.";
+                nullify();
+            }
+
+            result1 = Double.TryParse(kmhBox.Text, out a);
+            if (!result1) error_msg();
+
+            if (a != 0)
+            {
+                a *= 0.621371192;
+                a = System.Math.Round(a, 0);
+                resultLabel.Text = kmhBox.Text + " kmh is: " + System.Convert.ToString(a) + " mph.";
+                nullify();
+            }
+
+            result1 = Double.TryParse(mphBox.Text, out a);
+            if (!result1) error_msg();
+
+            if (a != 0)
+            {
+                a *= 1.609344;
+                a = System.Math.Round(a, 0);
+                resultLabel.Text = mphBox.Text + " mph is: " + System.Convert.ToString(a) + " kmh.";
+                nullify();
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version 1.0 \nBuild date: 7.12.2013 20:12\nInitial Release", "About");
+        }
+    }
+}
